7adb313764c6d184b44dbae9807183c07113c15d
Timothy Lin <timothy.lin@cylynx.io>
2021-07-18 18:19:43 +0800

    sync template

---
+++ data/siteMetadata.js
@@ -0,0 +1,57 @@
+const siteMetadata = {
+  title: 'Quasilinear Musings',
+  author: 'Timothy Lin',
+  headerTitle: 'Quasilinear Musings',
+  description: 'Thoughts from a wandering mind',
+  language: 'en-us',
+  siteUrl: 'https://www.timlrx.com',
+  siteRepo: 'https://github.com/timlrx/timlrx.com',
+  image: '/static/img/avatar.png',
+  socialBanner: '/static/img/twitter-card.png',
+  email: 'timothy.lin@alumni.ubc.ca',
+  github: 'https://github.com/timlrx',
+  twitter: 'https://twitter.com/timlrxx',
+  linkedin: 'https://www.linkedin.com/in/timlrx',
+  locale: 'en-US',
+  comment: {
+    provider: '', // supported providers: giscus, utterances, disqus
+    giscusConfig: {
+      repo: '', // username/repoName
+      // Visit the link below, enter your repo in the configuration section and copy the script data parameters
+      // Before that you should create a new Github discussions category with the Announcements type so that new discussions can only be created by maintainers and giscus
+      // https://giscus.app/
+      repositoryId: '',
+      category: '',
+      categoryId: '',
+      mapping: '', // supported options: pathname, url, title
+      reactions: '', // Emoji reactions: 1 = enable / 0 = disable
+      // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
+      metadata: '',
+      // theme example: light, dark, dark_dimmed, dark_high_contrast
+      // transparent_dark, preferred_color_scheme, custom
+      theme: '',
+      // theme when dark mode
+      darkTheme: '',
+      // If the theme option above is set to 'custom`
+      // please provide a link below to your custom theme css file.
+      // example: https://giscus.app/themes/custom_example.css
+      themeURL: '',
+    },
+    utterancesConfig: {
+      repo: '', // username/repoName
+      issueTerm: '', // supported options: pathname, url, title
+      label: '', // label (optional): Comment 💬
+      // theme example: github-light, github-dark, preferred-color-scheme
+      // github-dark-orange, icy-dark, dark-blue, photon-dark, boxy-light
+      theme: '',
+      // theme when dark mode
+      darkTheme: '',
+    },
+    disqus: {
+      // https://help.disqus.com/en/articles/1717111-what-s-a-shortname
+      shortname: '',
+    },
+  },
+}
+
+module.exports = siteMetadata
